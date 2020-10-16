a5a64a42d09cfe1c41d6a5c228d41ea229e0fc76
Jules FAUCHEUX <jfaucheux@lerebours.org>
2020-10-16 09:49:43 +0200

    Ajoutez des fichiers projet.

---
+++ ProjetMetier/GestionnaireDeCaves.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetMetier
+{
+    public class GestionnaireDeCaves
+    {
+        private Dictionary<string, List<Bouteille>> lesCaves;
+
+        public GestionnaireDeCaves()
+        {
+            LesCaves = new Dictionary<string, List<Bouteille>>();
+        }
+
+        public Dictionary<string, List<Bouteille>> LesCaves { get => lesCaves; set => lesCaves = value; }
+
+
+        
+        public void AjouterCave(string unNomDeCave,List<Bouteille> lesBouteillesDeLaCave)
+        {
+            LesCaves.Add(unNomDeCave,lesBouteillesDeLaCave);
+        }
+
+        public int NbBouteilles(string unNomDeCave)
+        {
+            int num = 0;
+            foreach (GestionnaireDeCaves gest in lesCaves)
+            {
+                foreach (Bouteille bou in gest.lesBouteillesDeLaCave)
+                {
+                    if (Couleur == "Rouges")
+                    {
+                        num++;
+                    }
+
+
+                    return num;
+        }
+
+        public int NbBouteillesDeRouges(string unNomDeCave)
+        {
+            int num = 0;
+            foreach (GestionnaireDeCaves gest in lesCaves)
+            {
+                foreach (Bouteille bou in gest.lesBouteillesDeLaCave)
+                {
+                    if (Couleur == "Rouges")
+                    {
+                        num++;
+                    }
+                    else(Couleur == "")
+                    {
+                        return 0;
+                    }
+                }
+            }
+
+            return num;
+        }
+
+        public double ValeurDeLaCave(string unNomDeCave)
+        {
+            // A vous de jouer
+            return 0;
+        }
+    }
+}
